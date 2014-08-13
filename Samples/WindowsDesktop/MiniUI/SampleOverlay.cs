19fec2c80b602fe74966b077fe577a904c049471
AvengerDr <lafcad1o>
2014-08-14 00:22:18 +0100

    [UI] Restored sample

--- Samples/WindowsDesktop/MiniUI/SampleOverlay.cs
+++ Samples/WindowsDesktop/MiniUI/SampleOverlay.cs
@@ -24,7 +24,7 @@ namespace MiniUI
             Panel panel1 = new Panel { Position = new Vector2(128, 128), Width = 480, Height = 320 };
             Panel panel2 = new Panel { Position = new Vector2(480, 64), Width = 256, Height = 200 };
             Label label = new Label { Position = new Vector2(8, 8), Text = "I'm a label.", Width = 200, Height = 48 };
-            Button button = new Button { Position = new Vector2(16, 64), Content = new Label { Text = "Highlighted", TextStyleClass = "Small" } };
+            Button button = new Button { Position = new Vector2(16, 64), Content = new Label { Text = "Button", TextStyleClass = "Small" } };
             FpsCounter fpsCounter = new FpsCounter { Position = new Vector2(16, 16) };
             button.Tap += (s, eventArgs) => ((Label)button.Content).Text = "Highlighted";
 
